aed2c257ec29e5a33911495e1c3ff68d82075a26
sunan-technology <codebase.sunantechnology@gmail.com>
2021-03-01 12:15:12 +0530

    init commit

---
+++ src/main/java/com/sunan/HotelPosApiApplication.java
@@ -0,0 +1,13 @@
+package com.sunan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelPosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelPosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunan/HotelPosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelPosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
